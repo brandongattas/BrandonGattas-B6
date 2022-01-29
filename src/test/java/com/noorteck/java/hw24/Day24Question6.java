f43bff554df482b60105fa61420b9b1ecd6cae36
Brandon <Brandon@10.0.0.12>
2022-01-28 21:25:55 -0500

    Posting HW for Day24.

---
+++ src/test/java/com/noorteck/java/hw24/Day24Question6.java
@@ -0,0 +1,54 @@
+package com.noorteck.java.hw24;
+
+public class Day24Question6 {
+	
+	public static void main(String[] args) {
+		int[] n1 = {6, 1, 2, 3};
+		System.out.println(checkNum(n1));
+		
+		int[] n2 = {13, 2, 3,4, 6, 1, 2, 3};
+		System.out.println(checkNum(n2));
+		
+		int[] n3 = {3, 3,0, 1, 4, 3 ,6};
+		System.out.println(checkNum(n3));
+		
+		int[] n4 = {2, 6, 2};
+		System.out.println(checkNum(n4));
+	}
+	
+	protected static boolean checkNum (int[] number) {
+		boolean result=false;
+		int n1=0;
+		int n2=0;
+		
+		for(int i=0; i<number.length; i++) {
+			if(number[i]==2) {
+				n1+=1;
+			} else if (number[i]==5){
+				n2+=1;
+			}
+		}
+		
+		if(n1>1 || n2>1) {
+			result=true;
+		}
+		
+		return result;
+	}
+
+}
+
+/*
+6. Write a method that takes an array of int parameter and return true if the array 
+contains value 2 twice, or value 5 twice.
+Access Modifier: protected
+Non-Access Modifier: static
+Return Type: boolean
+Method Name: checkNum
+Parameter1: int [] number
+Test Data: 
+checkNum ([6, 1, 2, 3]) ----- FALSE
+checkNum ([13, 2, 3,4, 6, 1, 2, 3]) ----- TRUE
+checkNum ([3, 3,0, 1, 4, 3 ,6]) ----- FALSE
+checkNum [2, 6, 2]) ----- TRUE
+*/
---
+++ src/test/java/com/noorteck/java/hw24/Day24Question7.java
@@ -0,0 +1,49 @@
+package com.noorteck.java.hw24;
+
+public class Day24Question7 {
+	
+	public static void main(String[] args) {
+		int[] n1 = {88,22,6,1,2,3,88,22,44,33};
+		System.out.println(getIndexNumber(n1, 3));
+		
+		int[] n2 = {1, 2, 3,22,44,33};
+		System.out.println(getIndexNumber(n2, 12));
+		
+		int[] n3 = {88,22,6, 1,8,0};
+		System.out.println(getIndexNumber(n3, 22));
+		
+		int[] n4 = {2, 3, 88,22,44};
+		System.out.println(getIndexNumber(n4, 44));
+		
+	}
+
+	public static int getIndexNumber (int[] number, int elementValue) {
+		int result=-1;
+		
+		for(int i=0; i<number.length; i++) {
+			if(number[i]==elementValue) {
+				result=i;
+			} 
+		}
+		
+		return result;
+	}
+}
+
+/**
+7. Write a method that takes 2 parameters. An array of int parameter and integer 
+parameter. Find the index number of an array element. Check if an array has the 
+element value passed as 2nd parameter. If it contains return the index number of that 
+element otherwise return -1. 
+Access Modifier: public
+Non-Access Modifier: static
+Return Type: int 
+Method Name: getIndexNumber
+Parameter1: int [] number
+Parameter1: int elementValue
+Test Data: 
+getIndexNumber ([88,22,6, 1, 2, 3, 88,22,44,33], 3) ----- 5
+getIndexNumber ([1, 2, 3,22,44,33], 12) ----- -1
+getIndexNumber ([88,22,6, 1,8,0], 22) ----- 1
+getIndexNumber ([2, 3, 88,22,44], 44) ----- 4
+*/
